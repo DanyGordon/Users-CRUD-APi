a41ad74d37db255418bb36d904449f3391c211d1
DanyGordon <52357936+DanyGordon@users.noreply.github.com>
2021-02-10 22:01:54 +0200

    Update users.controller.js
    Fix indent problems and bug with incorrect Location header in response

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -3,10 +3,10 @@ const Cars = require('../models/car.model');
 
 exports.getAll = async (req, res, next) => {
   	try {
-    	const users = await Users.find({}).populate('cars');
-    	res.status(200).json(users);
+    	    	const users = await Users.find({}).populate('cars');
+    	    	res.status(200).json(users);
   	} catch (error) {
-    	next(error);   
+    		next(error);   
   	}   
 }
 
@@ -26,7 +26,7 @@ exports.getById = async (req, res, next) => {
 exports.create = async (req, res, next) => {
 	try {
 		const createdUser = await Users.create(req.body);
-		res.set("Location", req.url + `/${createdUser._id}`);
+		res.location(getCurrentUrl(req) + createdUser._id);
 		res.status(201).end();
 	} catch (error) {
 		next(error);
@@ -59,9 +59,8 @@ exports.addUserCar = async (req, res, next) => {
 			const car = await Cars.create(req.body);
 			user.cars.push(car);
 			await user.save();
-			const updatedUser = await Users.findById(req.params.uid).populate('cars');
-			
-			res.status(200).json(updatedUser);
+			res.location(req.protocol + '://' + req.get('host') + '/cars/' + car._id)
+			res.status(201).end();
 		} else {
 			res.status(404).send(`User with id ${req.params.uid} not found!`);
 		}
@@ -85,4 +84,12 @@ exports.getUserCars = async (req, res, next) => {
 
 exports.methodNotSupported = async (req, res, next) => {
 	res.status(403).send(`${req.method} operations not supported on /user${req.url}!`)
+}
+
+function getCurrentUrl(req) {
+	let endpoint = req.originalUrl;
+	if (!endpoint.endsWith('/')) {
+		endpoint = req.originalUrl + '/'
+	}
+	return req.protocol + '://' + req.get('host') + endpoint;
 }
